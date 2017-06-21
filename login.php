b7ad54a3e117bec6c8a3c0406f45c970c09fe6a0
kimbeomsoo <crowjhdqjatn@naver.com>
2017-06-21 18:47:41 +0900

    login

---
+++ login.php
@@ -0,0 +1,38 @@
+<html  lang="ko-KR">
+<head>
+<title>조정 기록 관리 시스템</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="http://localhost/project/logincss.css">
+</head>
+<SCRIPT LAUNGUAGE='JAVASCRIPT'>
+function test(){
+alert("아이디와 비밀번호를 확인하세요");
+}
+function test2(){
+}</SCRIPT>
+<body background="cc.JPG">
+
+<br><br><br><br><br><br><br><br><br><br>
+<div align="center"><img src="1234.GIF" style="width:50mm; height:30mm">
+
+<td align="right">
+  <form action="login_ok.php" method="post">
+    <table align="center" >
+      <tr>
+        <td>
+          <table align="center" width="100">
+            <tr>
+              <td><input type="text" name="user_id" placeholder="아이디"  style="width:40mm;height:8mm" ></td>
+            </tr>
+            <tr>
+              <td><input type="password" placeholder="비밀번호"  name="user_pw" style="width:40mm;height:8mm"></td>
+            </tr>
+            </table>
+      </td>
+      <td><input type="image" src="lo.jpg" name="submit" border=0 style="width:20mm; height:17mm"></td>
+  </tr>
+  </table>
+  </form>
+
+</body>
+</html>
